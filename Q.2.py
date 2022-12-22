6616bebdcd426a22e80f6cbb7e4a7fbe04e6496f
Priyanshu Gaur <121242301+17PRIYANSHU17@users.noreply.github.com>
2022-12-22 20:08:02 +0530

    Add files via upload

---
+++ Q.2.py
@@ -0,0 +1,5 @@
+name="Priyanshu Gaur"
+sid=22105103
+dept="Electronics and Communication"
+cg=10.0
+print(" Hey,",name,"Here!\n","My SID is",sid,"\n","I am from",dept,"department and my CGPA is",cg)
